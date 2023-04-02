b29addb1b23915317e8a060751cd2483f75b00af
Gerin Aryo Praseta <gerin.aryo@efishery.com>
2023-04-02 21:51:37 +0700

    initial commit

---
+++ test.js
@@ -0,0 +1,12 @@
+// inisialisasi array
+let arr = [1, 2, 3];
+
+// 1. buat array baru, terus di input ke array baru nilai nya
+let newArr = [];
+
+for (let i = 0; i < arr.length; i++) {
+  console.log("array : " + arr[i] + " bla");
+  console.log(`array : ${arr[i]} bla`);
+  // if bukan bilangan prima atau bilangan prima
+  // append ke array baru
+}
